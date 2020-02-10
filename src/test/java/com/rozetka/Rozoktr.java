1dd919dd5e5c5ec891330a2051071f6f88653d90
mariia.zelinska <mariia.zelinska@thomsonreuters.com>
2020-02-11 00:33:34 +0200

    addTest: Fixed mutlithreading test run.

--- src/test/java/com/rozetka/Rozoktr.java
+++ src/test/java/com/rozetka/Rozoktr.java
@@ -1,21 +1,61 @@
 package com.rozetka;
 
-import com.rozetka.pageobjects.HomePO;
 import org.testng.annotations.Test;
 
 public class Rozoktr extends BaseTest {
     @Test
     public void testRe() {
-        System.out.println(new HomePO().getTitle());
+        System.out.println(getDriver().getTitle());
     }
 
     @Test
     public void testR1e() {
-        System.out.println(new HomePO().getTitle());
+        System.out.println(getDriver().getTitle());
     }
 
     @Test
     public void test2Re() {
-        System.out.println(new HomePO().getTitle());
+        System.out.println(getDriver().getTitle());
     }
+
+//    WebDriver driver;
+//
+//    @Test()
+//    public void testOnChromeWithBrowserStackUrl()
+//    {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.get("https://rozetka.com.ua/");
+//        driver.manage().window().maximize();
+//        System.out.println("this is the test related to chrome browserstack homepage"+ " " +Thread.currentThread().getId());
+//
+//    }
+//
+//    @Test()
+//    public void testOnChromeWithBrowserStackSignUp()
+//    {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.get("https://rozetka.com.ua/");
+//        driver.manage().window().maximize();
+//
+//        System.out.println("this is the test related to chrome browserstack login"+ " " +Thread.currentThread().getId());
+//
+//    }
+//
+//    @Test()
+//    public void testOnChromeWithBrowserStackSignUp1()
+//    {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.get("https://rozetka.com.ua/");
+//        driver.manage().window().maximize();
+//
+//        System.out.println("this is the test related to chrome browserstack login"+ " " +Thread.currentThread().getId());
+//
+//    }
+
 }
